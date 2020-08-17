0d0097ff3dd3e83662951cfcd49f416246bb6959
Matthew McDade <Matthew.McDade13@gmail.com>
2020-08-16 22:42:48 -0700

    Implement Shaders

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,5 +1,6 @@
 use crate::buffers::DataType;
 use std::ops::*;
+use std::collections::HashMap;
 use image::{open, DynamicImage};
 use image::imageops::flip_vertical;
 use crate::opengl::*;
@@ -95,9 +96,241 @@ impl Texture {
     }
 }
 
+#[derive(Debug, Copy, Clone)]
+pub enum ShaderType {
+    Vertex = gl::VERTEX_SHADER as isize,
+    Fragment = gl::FRAGMENT_SHADER as isize
+}
+
+pub struct Shader {
+    id: u32,
+    uniform_locations: std::collections::HashMap<String, i32>
+}
+
+const FRAG_TEMPLATE_VARS: &str = r"#version 330
+    out vec4 FragColor;
+    in vec2 TexCoord;
+    in vec4 Color;
+    in vec3 FragPos;
+
+    uniform sampler2D u_texture;
+";
+
+const FRAG_TEMPLATE_MAIN: &str = r"
+void main()
+{
+    FragColor = effect(Color, u_texture, TexCoord, FragPos);
+}
+";
+
+const VERT_TEMPLATE_DECLS: &str = r"#version 330
+    layout(location = 0) in vec3 l_pos;
+    layout(location = 1) in vec2 l_texCoords;
+    layout(location = 2) in vec4 l_color;
+
+    out vec2 TexCoord;
+    out vec4 Color;
+    out vec3 FragPos;
+
+    uniform mat4 u_matrixMVP;
+    uniform mat4 u_modelMatrix;
+    mat4 matrixMVP = u_matrixMVP;
+";
+
+const VERT_TEMPLATE_DECLS_INSTANCED: &str = r"#version 330
+    layout(location = 0) in vec3 l_pos;
+    layout(location = 1) in vec2 l_texCoords;
+    layout(location = 2) in vec4 l_color;
+    layout(location = 3) in mat4 l_matrixMVP;
+    layout(location = 7) in mat4 u_modelMatrix;
+
+    out vec2 TexCoord;
+    out vec4 Color;
+    out vec3 FragPos;
+    mat4 matrixMVP = l_matrixMVP;
+";
+
+const VERT_TEMPLATE_MAIN: &str = r"
+void main()
+{
+    TexCoord = l_texCoords;
+    Color = l_color;
+
+    FragPos = vec3(u_modelMatrix * vec4(l_pos, 1.0));
+
+    gl_Position = position(matrixMVP, l_pos);
+}
+";
+
+const DEFAULT_VERT: &str = r"#version 330
+layout(location = 0) in vec3 l_pos;
+layout(location = 1) in vec2 l_texCoords;
+layout(location = 2) in vec4 l_color;
+
+out vec2 TexCoord;
+out vec4 Color;
+out vec3 FragPos;
+
+uniform mat4 u_matrixMVP;
+uniform mat4 u_modelMatrix;
+
+void main()
+{
+    TexCoord = l_texCoords;
+    Color = l_color;
+    FragPos = vec3(u_modelMatrix * vec4(l_pos, 1.0));
+
+    gl_Position = u_matrixMVP * vec4(l_pos, 1.0);
+}
+";
+
+const DEFAULT_INSTANCED_VERT: &str = r"#version 330
+layout(location = 0) in vec3 l_pos;
+layout(location = 1) in vec2 l_texCoords;
+layout(location = 2) in vec4 l_color;
+layout(location = 3) in mat4 l_matrixMVP;
+layout(location = 7) in mat4 u_modelMatrix;
+
+out vec2 TexCoord;
+out vec4 Color;
+out vec3 FragPos;
+
+void main()
+{
+	TexCoord = l_texCoords;
+	Color = l_color;
+
+   FragPos = vec3(u_modelMatrix * vec4(l_pos, 1.0));
+
+	gl_Position = l_matrixMVP * vec4(l_pos, 1.0);
+}
+";
+
+const DEFAULT_FRAG: &str = r"#version 330
+out vec4 FragColor;
+
+in vec4 Color;
+in vec2 TexCoord;
+uniform sampler2D u_texture;
+
+void main()
+{
+	FragColor = texture(u_texture, TexCoord) * Color
+}";
+
+impl Shader {
+
+    pub fn from_file(vert_filename: &str, frag_filename: &str) -> Result<Self, String> {
+        let vshader = gl_compile_shader_from_file(vert_filename, ShaderType::Vertex)?;
+        let fshader = gl_compile_shader_from_file(frag_filename, ShaderType::Fragment)?;
+
+        let shaderid = gl_create_shader_program(vshader, fshader)?;
+        Ok(Shader::new(shaderid))
+    }
+
+    pub fn from_memory(vert: &str, frag: &str) -> Result<Self, String> {
+        let vshader = gl_compile_shader(vert, ShaderType::Vertex)?;
+        let fshader = gl_compile_shader(frag, ShaderType::Fragment)?;
+        let id = gl_create_shader_program(vshader, fshader)?;
+
+        Ok(Shader::new(id))
+    }
+
+    pub fn from_template(position: &str, effect: &str) -> Result<Self, String> {
+        let vert_full = Shader::concat_shader_sources(VERT_TEMPLATE_DECLS, position, VERT_TEMPLATE_MAIN);
+        let frag_full = Shader::concat_shader_sources(FRAG_TEMPLATE_VARS, effect, FRAG_TEMPLATE_MAIN);
+        let mut s = Shader::from_memory(&vert_full, &frag_full)?;
+        Shader::fill_template_uniforms(&mut s);
+        Ok(s)
+    }
+
+    pub fn from_vert_template(position: &str) -> Result<Self, String> {
+        let vert_full = Shader::concat_shader_sources(VERT_TEMPLATE_DECLS, position, VERT_TEMPLATE_MAIN);
+        let frag_full = DEFAULT_FRAG;
+        let mut s = Shader::from_memory(&vert_full, &frag_full)?;
+        Shader::fill_template_uniforms(&mut s);
+        Ok(s)
+    }
+    pub fn from_frag_template(effect: &str) -> Result<Self, String> {
+        let vert_full = DEFAULT_VERT;
+        let frag_full = Shader::concat_shader_sources(FRAG_TEMPLATE_VARS, effect, FRAG_TEMPLATE_MAIN);
+        let mut s = Shader::from_memory(&vert_full, &frag_full)?;
+        Shader::fill_template_uniforms(&mut s);
+        Ok(s)
+    }
+    
+    pub fn from_template_instanced(vert: &str, frag: &str) -> Result<Self, String> {
+        unimplemented!("Instancing in shaders not yet supported");
+    }
+
+    pub fn set_uniform_4f(&self, name: &str, floats: (f32, f32, f32, f32)) {
+        self.bind();
+        gl_set_uniform_4f(self.uniform_locations[name], floats);
+    }
+
+    pub fn set_uniform_3f(&self, name: &str, floats: (f32, f32, f32)) {
+        self.bind();
+        gl_set_uniform_3f(self.uniform_locations[name], floats);
+    }
+
+    pub fn set_uniform_2f(&self, name: &str, floats: (f32, f32)) {
+        self.bind();
+        gl_set_uniform_2f(self.uniform_locations[name], floats);
+    }
+
+    pub fn set_uniform_f(&self, name: &str, n: f32) {
+        self.bind();
+        gl_set_uniform_f(self.uniform_locations[name], n);
+    }
+
+    pub fn set_uniform_i(&self, name: &str, n: i32) {
+        self.bind();
+        gl_set_uniform_i(self.uniform_locations[name], n);
+    }
+
+    pub fn set_uniform_matrix(&self) {
+        self.bind();
+        unimplemented!("Matrix Operations not yet implemented");
+    }
+
+    pub fn register_uniform(&mut self, name: &str) {
+        let k = String::from(name);
+        let v = gl_get_uniform_location(self.id, name);
+        let _  = self.uniform_locations.insert(k, v);
+    }
+
+    pub fn bind(&self) {
+        unsafe { opengl().UseProgram(self.id) }
+    }
+
+    fn new(id: u32) -> Self {
+        Shader { id, uniform_locations: HashMap::new() }
+    }
+
+    fn fill_template_uniforms(s: &mut Shader) {
+        s.register_uniform("u_matrixMVP");
+        s.register_uniform("u_modelMatrix");
+    }
+
+    fn concat_shader_sources(a: &str, b: &str, c: &str) -> String {
+        let mut result = String::from(a);
+        result.push_str(b);
+        result.push_str(c); 
+        result
+    }
+}
+
+impl Drop for Shader {
+    
+    fn drop(&mut self) {
+        unsafe { opengl().DeleteProgram(self.id) }
+    }
+}
+
 impl Drop for Texture {
     
     fn drop(&mut self) { 
         unsafe { opengl().DeleteTextures(1, &self.id) }
     }
 }
+
--- src/opengl.rs
+++ src/opengl.rs
@@ -2,6 +2,7 @@
 use glutin::PossiblyCurrent;
 use std::ffi::CStr;
 use crate::buffers::VertexArray;
+use crate::graphics::{ShaderType, Shader};
 
 pub(crate) mod gl {
     include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
@@ -59,4 +60,104 @@ pub(crate) fn gl_gen_texture() -> u32 {
     let mut id = unsafe { std::mem::zeroed() };
     unsafe { opengl().GenTextures(1, &mut id) };
     id
+}
+
+pub(crate) fn gl_get_uniform_location(shader_id: u32, name: &str) -> i32 {
+    unsafe { opengl().GetUniformLocation(shader_id, name.as_ptr() as *const _) }
+}
+
+pub(crate) fn gl_set_uniform_4f(location: i32, floats: (f32, f32, f32, f32)) {
+    unsafe { opengl().Uniform4f(location, floats.0, floats.1, floats.2, floats.3) }
+}
+pub(crate) fn gl_set_uniform_3f(location: i32, floats: (f32, f32, f32)) {
+    unsafe { opengl().Uniform3f(location, floats.0, floats.1, floats.2) }
+}
+pub(crate) fn gl_set_uniform_2f(location: i32, floats: (f32, f32)) {
+    unsafe { opengl().Uniform2f(location, floats.0, floats.1) }
+}
+pub(crate) fn gl_set_uniform_f(location: i32, n: f32) {
+    unsafe { opengl().Uniform1f(location, n) }
+}
+// TODO :: Implement after we have figured out matrix situation...
+// pub(crate) fn gl_set_uniform_matrix(location: i32, floats: (f32, f32, f32, f32)) {
+
+// }
+pub(crate) fn gl_set_uniform_i(location: i32, n: i32) {
+    unsafe { opengl().Uniform1i(location, n) }
+}
+
+pub(crate) fn gl_compile_shader_from_file(path: &str, shader_type: ShaderType) -> Result<u32, String> {
+    let shader_source = match read_file(path) {
+        Ok(s) => s,
+        Err(e) => return Err(format!("FILE READ ERROR :: {}", e))
+    };
+    gl_compile_shader(&shader_source, shader_type)
+}
+
+
+pub(crate) fn read_file(path: &str) -> std::io::Result<String> {
+    use std::fs::File;
+    use std::io::BufReader;
+    use std::io::prelude::*;
+
+    let f = File::open(path)?;
+    let mut reader = BufReader::new(f);
+    let mut contents = String::new();
+    reader.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
+const GL_MAX_LOG_BUFFER_LENGTH: usize = 512;
+
+// TODO :: Implement a way to parse given shader source files so we can find and fill out all uniform locations on shader
+pub(crate) fn gl_compile_shader(source: &str, stype: ShaderType) -> Result<u32, String> {
+    let gl = opengl();
+    let sid = unsafe { gl.CreateShader(stype as u32) };
+    unsafe {
+        gl.ShaderSource(sid, 1, source.as_ptr() as *const *const i8, 0 as *const _);
+        gl.CompileShader(sid);
+
+        let mut success = std::mem::zeroed();
+        gl.GetShaderiv(sid, gl::COMPILE_STATUS, &mut success);
+
+        if success == 0 {
+
+            gl.DeleteShader(sid);
+            let mut info_log: Vec<u8> = vec![0, GL_MAX_LOG_BUFFER_LENGTH as u8];
+
+            gl.GetShaderInfoLog(sid, info_log.len() as i32, 0 as *mut _, info_log.as_mut_ptr() as *mut _);
+            let shader_error = String::from_utf8(info_log).unwrap();
+            return Err(format!("SHADER COMPILE ERROR\n\r--------------------\n\r{}", shader_error))
+        }
+    };        
+    Ok(sid)
+}
+
+pub(crate) fn gl_create_shader_program(vert_id: u32, frag_id: u32) -> Result<u32, String> {
+    unsafe {
+        let gl = opengl();
+        let id = gl.CreateProgram();
+
+        gl.AttachShader(id, vert_id);
+        gl.AttachShader(id, frag_id);
+        gl.LinkProgram(id);
+
+        let mut success = std::mem::zeroed();
+        gl.GetProgramiv(id, gl::LINK_STATUS, &mut success);
+
+        if success == 0 {
+            gl.DeleteProgram(id);
+
+            let mut info_log: Vec<u8> = vec![0, GL_MAX_LOG_BUFFER_LENGTH as u8];
+
+            gl.GetProgramInfoLog(id, info_log.len() as i32, 0 as *mut _, info_log.as_mut_ptr() as *mut _);
+            let shader_error = String::from_utf8(info_log).unwrap();
+            return Err(format!("SHADER PROGRAM LINK ERROR\n\r-------------------\n\r{}", shader_error))
+        }
+
+        gl.DeleteShader(vert_id);
+        gl.DeleteShader(frag_id);
+
+        Ok(id)
+    }
 }
