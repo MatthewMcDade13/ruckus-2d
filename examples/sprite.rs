af3d36ff4f476d5430d4c89043cea6dfaa2fa81e
MatthewMcDade13 <Matthew.McDade13@tuta.io>
2021-07-20 20:21:08 -0700

    cleanup example

--- examples/sprite.rs
+++ examples/sprite.rs
@@ -67,68 +67,68 @@ fn load_audio_manager() -> Result<AudioManager, kira::manager::error::SetupError
 }
 
 fn main() -> Result<(), ()> {
-    unsafe {
-        let el = EventLoop::new();
-        let wb = WindowBuilder::new().with_title("A fantastic window!");
-        
+    let el = EventLoop::new();
+    let wb = WindowBuilder::new().with_title("A fantastic window!");
     
-        let windowed_context =
-            ContextBuilder::new()
-            .with_double_buffer(Some(true))
-            .with_gl(glutin::GlRequest::Specific(glutin::Api::OpenGl, (3, 3)))
-            .with_gl_profile(glutin::GlProfile::Core)
-            .build_windowed(wb, &el)           
-            .unwrap();
     
-        let windowed_context = unsafe { windowed_context.make_current().unwrap() };
-
-
-        let mut audio = load_audio_manager().unwrap();
-        let mut sound_handle = audio.load_sound("./rainy_village_8_bit_lofi.mp3", SoundSettings::default()).unwrap();
-        let _ = sound_handle.play(InstanceSettings::default());
-        load_opengl(|p| windowed_context.get_proc_address(p));
-        
-        // let r = ruckus::Renderer::new(800, 600);
-
-        let tex = ruckus::graphics::Texture::from_file("./pepe-the-frog.jpg").unwrap();
-        let mut q = ruckus::sys::Quad::default();
-
-        let mut renderer = ruckus::Renderer::new(800,600);
+    let windowed_context =
+        ContextBuilder::new()
+        .with_double_buffer(Some(true))
+        .with_gl(glutin::GlRequest::Specific(glutin::Api::OpenGl, (3, 3)))
+        .with_gl_profile(glutin::GlProfile::Core)
+        .build_windowed(wb, &el)           
+        .unwrap();
     
-        let shader =  Shader::from_memory(VS_SRC, FS_SRC).unwrap();
+    let windowed_context = unsafe { windowed_context.make_current().unwrap() };
+
+    let mut audio = load_audio_manager().unwrap();
+    let mut sound_handle = audio.load_sound("./rainy_village_8_bit_lofi.mp3", SoundSettings::default()).unwrap();
+    let lofi_handle = sound_handle.play(InstanceSettings::default()).unwrap();
 
 
-        el.run(move |event, _, control_flow| {
-            match event {
-                Event::LoopDestroyed => return,
-                Event::WindowEvent { event, .. } => match event {
-                    WindowEvent::Resized(physical_size) => {
-                        windowed_context.resize(physical_size)
-                    }
-                    WindowEvent::CloseRequested => {
-                        *control_flow = ControlFlow::Exit
-                    }
-                    _ => (),
-                },
-                Event::RedrawRequested(_) => {
-                } 
+    load_opengl(|p| windowed_context.get_proc_address(p));
+    
+    // let r = ruckus::Renderer::new(800, 600);
+
+    let tex = ruckus::graphics::Texture::from_file("./pepe-the-frog.jpg").unwrap();
+    let mut q = ruckus::sys::Quad::default();
+
+    let mut renderer = ruckus::Renderer::new(800,600);
+
+    let shader =  Shader::from_memory(VS_SRC, FS_SRC).unwrap();
+
+
+    el.run(move |event, _, control_flow| {
+        match event {
+            Event::LoopDestroyed => return,
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::Resized(physical_size) => {
+                    windowed_context.resize(physical_size)
+                }
+                WindowEvent::CloseRequested => {
+                    *control_flow = ControlFlow::Exit
+                }
                 _ => (),
-            }
+            },
+            Event::RedrawRequested(_) => {
+            } 
+            _ => (),
+        }
 
-            let mut model = glm::Mat4::identity();
-            model = glm::rotate(&model, ruckus::sys::radians(90.0), &glm::vec3(0., 0., 1.));
+        let mut model = glm::Mat4::identity();
+        model = glm::rotate(&model, ruckus::sys::radians(90.0), &glm::vec3(0., 0., 1.));
 
-            renderer.camera.position = glm::vec3(4., 3., 3.);            
-            renderer.camera.look_direction = glm::vec3(-4., -3., -3.);            
-            let x = renderer.projection() * renderer.view() * model;
-            shader.set_uniform_matrix("u_mvp", &x);
+        renderer.camera.position = glm::vec3(4., 3., 3.);            
+        renderer.camera.look_direction = glm::vec3(-4., -3., -3.);            
+        let x = renderer.projection() * renderer.view() * model;
+        shader.set_uniform_matrix("u_mvp", &x);
 
-            renderer.clear(0.2, 0.3, 0.3, 1.0);
-            shader.apply();
-            renderer.draw_quad(&q, &tex);
+        renderer.clear(0.2, 0.3, 0.3, 1.0);
+        shader.apply();
+        renderer.draw_quad(&q, &tex);
 
 
-            windowed_context.swap_buffers().unwrap();
-        });
-    }
+        windowed_context.swap_buffers().unwrap();
+    });
 }
+
